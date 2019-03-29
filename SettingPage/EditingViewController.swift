f8fce65612047016144a6707efa27c63995e4de7
ng wing yiu <nwy0206@gmail.com>
2019-03-29 19:55:28 +0800

    Add EditingViewController with view set up

---
+++ SettingPage/EditingViewController.swift
@@ -0,0 +1,33 @@
+//
+//  EditingViewController.swift
+//  SettingPage
+//
+//  Created by WY NG on 29/3/2019.
+//  Copyright © 2019 lumanmann. All rights reserved.
+//
+
+import UIKit
+
+class EditingViewController: UIViewController {
+    
+    @IBOutlet weak var discriptionLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
