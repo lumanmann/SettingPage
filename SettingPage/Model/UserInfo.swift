47576f9a80ff834596a66380f0375db741876208
ng wing yiu <nwy0206@gmail.com>
2019-03-29 19:41:32 +0800

    Add UserInfo model class including print function

---
+++ SettingPage/Model/UserInfo.swift
@@ -0,0 +1,47 @@
+//
+//  UserInfo.swift
+//  SettingPage
+//
+//  Created by WY NG on 25/3/2019.
+//  Copyright © 2019 lumanmann. All rights reserved.
+//
+
+import Foundation
+
+
+class UserInfo {
+    var name: String?
+    var account: String?
+    var password: String?
+    var isEditable: Bool = false
+    var favouriteFood = [String]()
+    
+    
+    init() {
+      
+    }
+    
+    init(name: String? = nil, account: String? = nil, password: String? = nil, favouriteFood: [String]? = nil) {
+        self.name = name
+        self.account = account
+        self.password = password
+        if let foods = favouriteFood{
+            self.favouriteFood = foods
+        }
+    }
+    
+    
+    func printAll() {
+        print("===")
+        print(name ?? "No name" ,
+              "\(self.account ?? "No account")",
+            "\(self.password ?? "No password")",
+            "\(isEditable)",
+            separator: ", ", terminator: "\n")
+        for food in favouriteFood {
+            print(food)
+        }
+        print("===")
+    }
+    
+}
